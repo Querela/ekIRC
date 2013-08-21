da3a45b234baaceb89fd7eaf28492fee7861ad49
Querela <ek.92@arcor.de>
2013-08-21 17:51:18 +0200

    ip to and from long conversion
    NOTE: InetAddress does it as well ...

--- src/de/ekdev/ekirc/core/IRCDCCManager.java
+++ src/de/ekdev/ekirc/core/IRCDCCManager.java
@@ -28,6 +28,57 @@ public class IRCDCCManager
 
     // ------------------------------------------------------------------------
 
+    // http://www.silisoftware.com/tools/ipconverter.php
+
+    public static byte[] longToIP(long address)
+    {
+        byte[] ip = new byte[4];
+
+        for (int i = 3; i >= 0; i--)
+        {
+            ip[i] = (byte) (address % 256);
+            address = address / 256;
+        }
+
+        return ip;
+    }
+
+    public static long ipToLong(byte[] addressBytes)
+    {
+        if (Objects.requireNonNull(addressBytes, "addressBytes must not be null!").length != 4)
+        {
+            throw new IllegalArgumentException("Argument addressBytes must be an byte array with length 4!");
+        }
+
+        long address = 0;
+
+        for (int i = 0; i < 4; i++)
+        {
+            address <<= 8; // *= 256
+            address += addressBytes[i] & 0xFF; // signed byte to unsigned int
+        }
+
+        return address;
+    }
+
+    public static String ipToString(byte[] addressBytes)
+    {
+        if (Objects.requireNonNull(addressBytes, "addressBytes must not be null!").length != 4)
+        {
+            throw new IllegalArgumentException("Argument addressBytes must be an byte array with length 4!");
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 4; i++)
+        {
+            if (i != 0) sb.append('.');
+            sb.append(addressBytes[i] & 0xFF);
+        }
+        return sb.toString();
+    }
+
+    // ------------------------------------------------------------------------
+
     public final IRCNetwork getIRCNetwork()
     {
         return this.ircNetwork;
---
+++ test/de/ekdev/ekirc/core/test/SomeTests.java
@@ -0,0 +1,63 @@
+/**
+ * SomeTests.java
+ */
+package de.ekdev.ekirc.core.test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import de.ekdev.ekirc.core.IRCDCCManager;
+
+/**
+ * @author ekDev
+ */
+public class SomeTests
+{
+    public SomeTests()
+    {
+    }
+
+    public static void main(String[] args) throws UnknownHostException
+    {
+        long address = 1657180742;
+
+        System.out.println("// Input long (ip address)");
+        System.out.println("address      = " + address);
+
+        byte[] addressBytes = IRCDCCManager.longToIP(address);
+
+        System.out.println("// Converted long to ip address bytes (neg. values += 256)");
+        System.out.print("address      = ");
+        for (int i = 0; i < 4; i++)
+        {
+            if (i != 0) System.out.print('.');
+            // System.out.print((addressBytes[i] < 0) ? addressBytes[i] + 256 : addressBytes[i]);
+            System.out.print(addressBytes[i] & 0xFF);
+        }
+        System.out.println();
+
+        System.out.println("// Output address bytes with StringBuilder and magic");
+        System.out.println("address (sb) = " + IRCDCCManager.ipToString(addressBytes));
+
+        long address2 = IRCDCCManager.ipToLong(addressBytes);
+
+        System.out.println("// Convert ip address bytes to long");
+        System.out.println("address      = " + address2);
+
+        // --------------------------------------------------------------------
+
+        System.out.println("// Get InetAddress by address bytes");
+        InetAddress ia = InetAddress.getByAddress(addressBytes);
+        System.out.println("address (ia) = " + ia.getHostAddress());
+        System.out.println("name    (ia) = " + ia.getHostName());
+
+        System.out.println("// Get InetAddress by long");
+        ia = InetAddress.getByName("" + address);
+        System.out.println("address (ia) = " + ia.getHostAddress());
+        System.out.println("name    (ia) = " + ia.getHostName());
+
+        System.out.println("// Get ip from address bytes from InetAddress");
+        byte[] addressBytes2 = ia.getAddress();
+        System.out.println("address (sb) = " + IRCDCCManager.ipToString(addressBytes2));
+    }
+}
